deedf47ccb418d70aa22ac58b1d59cbad9012f06
Casey K <tehcnicolour@gmail.com>
2019-02-01 00:43:16 -0800

    replacerenderer styled comps

--- gatsby-srr.tsx
+++ gatsby-srr.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
 import { store } from './src/store'
 
-exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+  const sheet = new ServerStyleSheet()
   const ConnectedBody = () => (
     <Provider store={store}>
-      {bodyComponent}
+      <StyleSheetManager sheet={sheet.instance}>
+        {bodyComponent}
+      </StyleSheetManager>
     </Provider>
   )
   replaceBodyHTMLString(renderToString(<ConnectedBody />))
+  setHeadComponents([sheet.getStyleElement()])
 }
